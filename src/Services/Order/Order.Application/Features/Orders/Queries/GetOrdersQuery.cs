bb2065b2b86e509404b60125e65ee734238aaf4d
lehoa08121998@gmail.com <lehoa08121998@gmail.com>
2022-02-19 00:36:47 +0700

    [Order.Application][Features][Orders] Applying CQRS. Implementing Queries

---
+++ src/Services/Order/Order.Application/Features/Orders/Queries/GetOrdersQuery.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MediatR;
+using Order.Application.Contracts.Persistence;
+
+namespace Order.Application.Features.Orders.Queries
+{
+    public class GetOrdersQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
---
+++ src/Services/Order/Order.Application/Features/Orders/Queries/GetOrdersQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using Order.Application.Contracts.Persistence;
+
+namespace Order.Application.Features.Orders.Queries
+{
+    public class GetOrdersQueryHandler : IRequestHandler<GetOrdersQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersQuery request,
+            CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
