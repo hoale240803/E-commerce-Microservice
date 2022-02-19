59feb7c715b65e1f47da50fcd245f3db80788488
lehoa08121998@gmail.com <lehoa08121998@gmail.com>
2022-02-19 15:40:50 +0700

    [Order.Infrastructure] Adding repositories, email service

---
+++ src/Services/Order/Order.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.Application.Contracts.Persistence;
+using Order.Infrastructure.Persistence;
+
+namespace Order.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Domain.Entities.Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Domain.Entities.Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                    .Where(o => o.UserName == userName)
+                                    .ToListAsync();
+            return orderList;
+        }
+    }
+}
