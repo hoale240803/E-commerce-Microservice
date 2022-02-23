a83460f50c21e33dec0025a685ae083080845ad9
lehoa08121998@gmail.com <lehoa08121998@gmail.com>
2022-02-23 07:47:42 +0700

    [EventBus] Updating Order Mapper

---
+++ src/Services/Order/Order.API/Mapping/OrderProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using EventBus.Messages.Events;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+
+namespace Order.API.Mapping
+{
+    public class OrderingProfile : Profile
+    {
+        public OrderingProfile()
+        {
+            CreateMap<CheckoutOrderCommand, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
